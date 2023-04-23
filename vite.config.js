35222e5600d01f36d458548a215bc8820d88acf7
rtroadec <ryan.troadec@etudiant.univ-rennes1.fr>
2023-04-23 22:13:20 +0200

    Projet fini???

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Site-RickandMorty/", // pour déployer sur github pages
   plugins: [vue()],
 })
 
-base: "/Site-RickandMorty/" // pour déployer sur github pages
+
